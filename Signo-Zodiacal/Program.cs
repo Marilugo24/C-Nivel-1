60868cbe81d0900b409b80b21e8d8d50194e92e5
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-05-17 10:16:07 -0400

    finalizado Signo Zodiacal

---
+++ Signo-Zodiacal/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Introduzca su dia de nacimiento");
+int Dia = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca su mes de nacimiento");
+int Mes = int.Parse(Console.ReadLine());
+
+
+if(Dia>22 && Mes>=11){
+    Console.WriteLine("Tu signo zodiacal es Sagitario");
+}
+
+else if(Dia<21 && Mes<=11){
+    Console.WriteLine("Tu signo es Escorpio");
+}
+
+if(Dia>28 && Mes<11){
+    Console.WriteLine("Tu signo es Capricornio");
+}
