f6db0f23e7a90b333d48d57e520b2ea3887e0b7b
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-05-17 07:44:30 -0400

    Finalizado

---
+++ Calculadora/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Introduzca un número");
+int valor1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca otro número");
+int valor2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La suma es:"+(valor1+valor2));
+
+Console.WriteLine("Introduzca un número");
+int valor3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca otro número");
+int valor4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La resta es:"+(valor3-valor4));
+
+Console.WriteLine("Introduzca un número");
+int valor5 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca otro número");
+int valor6 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La multiplicación es:"+(valor5*valor6));
+
+Console.WriteLine("Introduzca un numero");
+int valor7 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca otro numero");
+int valor8 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La división es:"+(valor7/valor8));
