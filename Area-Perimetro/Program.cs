0f39706b3632c71b6d08c361be54d7fcef580f78
Maria Lugo <mariafernanda.lugobolivar@gmail.com>
2024-05-17 09:08:12 -0400

    finalizado Area y Perimetro

---
+++ Area-Perimetro/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Introduzca la base del rectángulo");
+int Base1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduzca la altura del rectángulo");
+int Altura = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El perímetro del rectángulo es:" +(Base1+Altura+Base1+Altura));
+
+Console.WriteLine("El área del rectángulo es:" +(Base1*Altura));
